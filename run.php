9fbad28fe7d7812bcf6059d1ff650e76e7be7c17
Erik Zigo <erik.zigo@keboola.com>
2017-09-07 15:45:07 +0200

    component endpoint with basic workspace application

---
+++ run.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Application endpoint
+ *
+ * @author Erik Zigo <erik.zigo@keboola.com>
+ */
+use Keboola\DbWriter\Exception\ApplicationException;
+use Keboola\DbWriter\Exception\UserException;
+use Keboola\DbWriter\Logger;
+use Keboola\DbWriter\Snowflake\Application;
+use Monolog\Handler\NullHandler;
+use Symfony\Component\Yaml\Yaml;
+
+define('APP_NAME', 'wr-db-snowflake-v2');
+define('ROOT_PATH', __DIR__);
+
+require_once(dirname(__FILE__) . "/vendor/keboola/db-writer-common/bootstrap.php");
+
+$logger = new Logger(APP_NAME);
+
+$action = 'run';
+
+try {
+    if (!getenv('KBC_TOKEN')) {
+        throw new UserException("Missing KBC API token");
+    }
+    if (!getenv('KBC_URL')) {
+        throw new UserException("Missing KBC API url");
+    }
+
+    $client = new \Keboola\StorageApi\Client([
+        'token' => getenv('KBC_TOKEN'),
+        'url' => getenv('KBC_URL'),
+        'backoffMaxTries' => 0,
+    ]);
+
+    $arguments = getopt("d::", ["data::"]);
+    if (!isset($arguments["data"])) {
+        throw new UserException('Data folder not set.');
+    }
+
+    $configFile = new SplFileInfo($arguments["data"] . "/config.yml");
+    if (!$configFile->isFile()) {
+        throw new UserException('Missing configuration file in data folder.');
+    }
+
+    $config = Yaml::parse(file_get_contents($configFile));
+    $config['parameters']['data_dir'] = $arguments['data'];
+    $action = isset($config['action']) ? $config['action'] : $action;
+
+    // app init
+    if (isset($config['parameters']['workspaceId'])) {
+        $app = new Application\Workspace($client, $logger);
+//   @TODO implement
+//    } elseif (isset($config['parameters']['db'])) {
+//        $app = new Application\Connect($client, $logger);
+    } else {
+        throw new UserException("Unsupported configuration. Missing 'workspaceId' or 'db' parameters");
+    }
+
+    if ($action !== 'run') {
+        $logger->setHandlers(array(new NullHandler(Logger::INFO)));
+    }
+
+    echo json_encode($app->run($action, $config));
+} catch (UserException $e) {
+    $logger->log('error', $e->getMessage(), (array) $e->getData());
+
+    if ($action !== 'run') {
+        echo $e->getMessage();
+    }
+
+    exit(1);
+} catch (ApplicationException $e) {
+    $logger->log('error', $e->getMessage(), (array) $e->getData());
+    exit($e->getCode() > 1 ? $e->getCode(): 2);
+} catch (\Exception $e) {
+    echo "\033[31mApp error\033[0m";
+    $logger->log('error', $e->getMessage(), [
+        'errFile' => $e->getFile(),
+        'errLine' => $e->getLine(),
+    //    'trace' => $e->getTrace()
+    ]);
+    exit(2);
+}
+exit(0);
---
+++ src/Keboola/DbWriter/Snowflake/Application/Workspace.php
@@ -0,0 +1,100 @@
+<?php
+namespace Keboola\DbWriter\Snowflake\Application;
+
+use Keboola\DbWriter\Exception\UserException;
+use Keboola\DbWriter\Logger;
+use Keboola\DbWriter\Snowflake\Configuration\WorkspaceDefinition;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\ClientException;
+use Keboola\StorageApi\Workspaces;
+
+class Workspace extends Base
+{
+    public function __construct(Client $sapiClient, Logger $logger)
+    {
+        parent::__construct($sapiClient, $logger, new WorkspaceDefinition());
+    }
+
+    protected function testConnectionAction(array $params)
+    {
+        $workspaces = new Workspaces($this->sapiClient);
+        // @FIXME validate if workspace is assigned to our compoennt
+        try {
+            $workspaces->getWorkspace($params['workspaceId']);
+        } catch (ClientException $e) {
+            throw new UserException($e->getMessage());
+        }
+
+        return [
+            'status' => 'success',
+        ];
+    }
+
+    protected function testWorkspaceAction(array $params)
+    {
+        $workspaces = new Workspaces($this->sapiClient);
+
+        try {
+            $workspaces->getWorkspace($params['workspaceId']);
+        } catch (ClientException $e) {
+            throw new UserException($e->getMessage());
+        }
+
+        return [
+            'status' => 'success',
+        ];
+    }
+
+    protected function runAction(array $params)
+    {
+        $workspaces = new Workspaces($this->sapiClient);
+
+        $uploaded = [];
+        $tables = array_filter($params['tables'], function ($table) {
+            return ($table['export']);
+        });
+
+        foreach ($tables as $table) {
+            $this->logger->info(sprintf('Trying load table: "%s"', $table['tableId']));
+            $options = [
+                'input' => [
+                    [
+                        'source' => $table['tableId'],
+                        'destination' => $table['dbName'],
+                        'incremental' => $table['incremental'],
+                        'datatypes' => [],
+                        'columns' => [],
+                    ],
+                ],
+            ];
+
+
+            foreach ($table['items'] as $item) {
+                $options['input'][0]['columns'][] = $item['name'];
+
+                $options['input'][0]['datatypes'][$item['name']] = [
+                    'column' => $item['name'],
+                    'type' => $item['type'],
+                    'length' => $item['size'],
+                    'nullable' => $item['nullable'],
+                    'convertEmptyValuesToNull' => $item['nullable'],
+                ];
+            }
+
+            try {
+                $hovno = $workspaces->loadWorkspaceData($params['workspaceId'], $options);
+                var_dump($hovno);
+            } catch (ClientException $e) {
+                //@TODO error code beeter conversion
+                throw new UserException($e->getMessage());
+            }
+
+            $uploaded[] = $table['tableId'];
+        }
+
+        return [
+            'status' => 'success',
+            'uploaded' => $uploaded
+        ];
+    }
+}
